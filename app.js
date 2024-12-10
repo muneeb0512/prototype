4c5e0b876c5b5b98bdfc7694ca1f061e2a58e9e4
muneeb <muneeburrehman501@gmail.com>
2024-12-10 14:57:19 -0800

    separated express app out of server so that it could be used in ened to end test files

--- app.js
+++ app.js
@@ -1,38 +1,33 @@
 
+require('dotenv').config();
 const express = require('express');
-const app = express();
-const expressLayouts = require('express-ejs-layouts');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const flash = require('connect-flash');
 const path = require('path');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const passport = require('passport');
-require('dotenv').config();
-
-var partials = require('express-partials');
+const expressLayouts = require('express-ejs-layouts');
 
 
-require('./config/passport')(passport);
+const app = express();
 
 
 const db = require('./config/db');
-
-
 mongoose.connect(db.url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected'))
+})
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 
 app.use(expressLayouts);
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(flash());
-
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'your-secret-key',
@@ -44,22 +39,15 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
 app.use((req, res, next) => {
-    res.locals.activePage = '';
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    res.locals.info_msg = req.flash('info_msg'); 
-    res.locals.error = req.flash('error');
-    res.locals.user= req.user;
-    next();
-  });
-app.set('view engine', 'ejs');
-
-
-
-
+  res.locals.activePage = '';
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.info_msg = req.flash('info_msg');
+  res.locals.error = req.flash('error');
+  res.locals.user = req.user;
+  next();
+});
 
 
 const authRoutes = require('./routes/auth');
@@ -69,7 +57,4 @@ app.use('/', authRoutes);
 app.use('/ssh', sshRoutes);
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
